e589a6cd6f265ea5e58cce5249f52c7dc536a23b
sairasanadi <120391374+sairasanadi@users.noreply.github.com>
2023-04-29 14:50:44 +0530

    Add files via upload

---
+++ Weather-app-React--main/src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "797f7fae09msh175636030a03a85p1a9f0ajsn25337bdf6000",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0d6722fa86a08b7a8c41b6a023c5561d"; // enter your key from openweather API
